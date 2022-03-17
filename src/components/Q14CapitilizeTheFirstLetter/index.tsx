e5be95b5241230aa8397631b686b7f63a709c5c0
Breno <osalesbreno@gmail.com>
2022-03-17 15:00:14 -0300

    Correção da questão 14

--- src/components/Q14CapitilizeTheFirstLetter/index.tsx
+++ src/components/Q14CapitilizeTheFirstLetter/index.tsx
@@ -5,7 +5,9 @@ interface IQ14CapitilizeTheFirstLetterProps {
 const Q14CapitilizeTheFirstLetter: React.FC<
     IQ14CapitilizeTheFirstLetterProps
 > = ({ value }) => (
-    <span>{value.charAt(0).toUpperCase() + value.slice(1).toLowerCase()}</span>
+    <span>{`${
+        value.charAt(0).toUpperCase() + value.slice(1).toLowerCase()
+    }`}</span>
 );
 
 export default Q14CapitilizeTheFirstLetter;
