b19fc720d9c5614a2571e767a4cd4850fc5a9232
Breno <osalesbreno@gmail.com>
2022-03-16 21:25:18 -0300

    Questão 5

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import './App.css';
 import Q1String from './components/Q1String';
 import Q2Uppercase from './components/Q2Uppercase';
 import Q3Lowercase from './components/Q3Lowercase';
+import Q5FirstChar from './components/Q5FirstChar';
 
 const App: React.FC = () => {
     return (
@@ -43,7 +44,9 @@ const App: React.FC = () => {
                 5. Crie um componente que recebe uma prop &quot;value&quot;, do
                 tipo string, e retorna a primeira letra dessa string.
             </h2>
-            <div className="card p-3 mb-4">{/* Insira a resposta aqui */}</div>
+            <div className="card p-3 mb-4">
+                <Q5FirstChar value="TESTE" />
+            </div>
 
             <h2 className="fs-6">
                 6. Crie um componente que recebe uma prop &quot;value&quot;, do
