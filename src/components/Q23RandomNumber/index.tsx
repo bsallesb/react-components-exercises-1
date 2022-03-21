1a77e963540267734af5ac379a0e2efc26115fcf
Breno <osalesbreno@gmail.com>
2022-03-21 11:27:02 -0300

    Correção da questão 23

--- src/components/Q23RandomNumber/index.tsx
+++ src/components/Q23RandomNumber/index.tsx
@@ -1,4 +1,4 @@
-const Q23RandomNumber: React.FC = () => {
-    return <span>{Math.floor(Math.random() * (10 + 1))}</span>;
-};
+const Q23RandomNumber: React.FC = () => (
+    <span>{Math.floor(Math.random() * (10 + 1))}</span>
+);
 export default Q23RandomNumber;
