967ae259fdd2dfaa13db89a4ec60da44cef274e6
Breno <osalesbreno@gmail.com>
2022-03-29 12:50:06 -0300

    Questão 46

--- src/components/Q46AddressByZipCode/index.tsx
+++ src/components/Q46AddressByZipCode/index.tsx
@@ -1,31 +1,48 @@
-import { useCallback, useEffect, useState } from 'react';
+import { useCallback, useMemo, useState } from 'react';
+import InputMask from 'react-input-mask';
+
 import { ZipCodeType } from '../../@types/ZipCode';
 import Api from '../../services/Api';
 
 const Q46AddressByZipCode: React.FC = () => {
     const [zipCode, setZipCode] = useState('');
     const [address, setAddress] = useState<ZipCodeType | null>(null);
+    const [isLoading, setIsLoading] = useState(false);
 
-    const getAddress = useCallback(
-        async (zipCodeNumber: string): Promise<void> => {
-            Api.get(`https://viacep.com.br/ws/${zipCodeNumber}/json/`)
-                .then(response => setAddress(response.data ?? null))
-                .catch()
-                .finally();
-        },
+    const loading = useMemo(
+        () => (
+            <div className="d-flex mt-2">
+                <div className="spinner-grow me-2" role="status">
+                    <span className="visually-hidden">Loading...</span>
+                </div>
+                <div className="spinner-grow me-2" role="status">
+                    <span className="visually-hidden">Loading...</span>
+                </div>
+                <div className="spinner-grow" role="status">
+                    <span className="visually-hidden">Loading...</span>
+                </div>
+            </div>
+        ),
         []
     );
 
-    useEffect(() => {
-        getAddress(zipCode ?? '');
-        // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, []);
+    const getAddress = useCallback(() => {
+        const cleanZipCode = zipCode.replace('-', '');
+
+        if (cleanZipCode.length > 0) {
+            Api.get(`https://viacep.com.br/ws/${cleanZipCode}/json/`)
+                .then(response => setAddress(response.data ?? null))
+                .catch()
+                .finally(() => setIsLoading(false));
+        }
+    }, [zipCode]);
 
     return (
         <div>
             <div className="d-flex col-6 align-items-center mb-2">
                 <p className="m-0 me-2">CEP:</p>
-                <input
+                <InputMask
+                    mask="99999-999"
                     type="text"
                     placeholder="exemplo: 37400-000"
                     value={zipCode}
@@ -35,24 +52,35 @@ const Q46AddressByZipCode: React.FC = () => {
                 <button
                     type="button"
                     className="w-50 btn btn-primary"
-                    onClick={() => getAddress(zipCode.replace('-', ''))}
+                    onClick={() => {
+                        setIsLoading(true);
+                        getAddress();
+                    }}
                 >
                     Buscar endereço
                 </button>
             </div>
-            <div className="d-flex flex-column">
-                {address?.logradouro && (
-                    <span>Endereço: {address?.logradouro}</span>
-                )}
-                {address?.bairro && <span>Bairro: {address?.bairro}</span>}
-                {address?.complemento && (
-                    <span>Complemento: {address?.complemento}</span>
-                )}
-                {address?.cep && <span>CEP: {address?.cep}</span>}
-                {address?.localidade && (
-                    <span>
-                        Cidade: {address?.localidade} - {address?.uf}
-                    </span>
+            <div>
+                {isLoading ? (
+                    loading
+                ) : (
+                    <div className="d-flex flex-column">
+                        {address?.logradouro && (
+                            <span>Endereço: {address?.logradouro}</span>
+                        )}
+                        {address?.bairro && (
+                            <span>Bairro: {address?.bairro}</span>
+                        )}
+                        {address?.complemento && (
+                            <span>Complemento: {address?.complemento}</span>
+                        )}
+                        {address?.cep && <span>CEP: {address?.cep}</span>}
+                        {address?.localidade && (
+                            <span>
+                                Cidade: {address?.localidade} - {address?.uf}
+                            </span>
+                        )}
+                    </div>
                 )}
             </div>
         </div>
