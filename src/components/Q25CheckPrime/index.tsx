a8e3328248262acbae4d590ced4e03b59a011d8c
Breno <osalesbreno@gmail.com>
2022-03-30 16:12:02 -0300

    Incluindo useMemo na questão 25

--- src/components/Q25CheckPrime/index.tsx
+++ src/components/Q25CheckPrime/index.tsx
@@ -1,15 +1,21 @@
+import { useMemo } from 'react';
+
 interface IQ25CheckPrimeProps {
     value: number;
 }
 
 const Q25CheckPrime: React.FC<IQ25CheckPrimeProps> = ({ value }) => {
-    if (value < 2) return <span>Não</span>;
+    const isPrime = useMemo(() => {
+        if (value < 2) return false;
 
-    for (let i = 2; i < value; i += 1) {
-        if (value % i === 0) {
-            return <span>Não</span>;
+        for (let i = 2; i < value; i += 1) {
+            if (value % i === 0) {
+                return false;
+            }
         }
-    }
-    return <span>Sim</span>;
+        return true;
+    }, [value]);
+
+    return <span>{isPrime ? 'Sim' : 'Não'}</span>;
 };
 export default Q25CheckPrime;
