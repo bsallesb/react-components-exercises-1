538dfac1c5dd879a68924c3015c4498b68062c5f
Breno <osalesbreno@gmail.com>
2022-04-01 12:55:48 -0300

    Questão 9

--- src/components/Q9RepeatTextSeparatedByComma/index.tsx
+++ src/components/Q9RepeatTextSeparatedByComma/index.tsx
@@ -8,14 +8,16 @@ interface Q9RepeatTextSeparatedByCommaProps {
 const Q9RepeatTextSeparatedByComma: React.FC<
     Q9RepeatTextSeparatedByCommaProps
 > = ({ text, factor }) => {
-    const texts = [];
     const repeatText = useMemo(() => {
+        const texts = [];
         for (let i = 0; i <= factor; i += 1) {
             texts.push(text);
         }
+        return texts;
+        // eslint-disable-next-line react-hooks/exhaustive-deps
     }, []);
 
-    return <div>({repeatText})</div>;
+    return <div>{repeatText.join(', ')}</div>;
 };
 
 export default Q9RepeatTextSeparatedByComma;
